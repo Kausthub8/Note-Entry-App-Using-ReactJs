1d54094d52a8926aa8e99806ad0d64ab381699e4
Dude-1999 <kausthub1369@gmail.com>
2020-04-30 19:02:13 +0530

    Add files via upload

---
+++ src/Lists.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Lists.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import FlipMove from 'react-flip-move';
+
+function Lists(props){
+    const items = props.items;
+    const lists = items.map(item =>
+   {
+       return <div className="lists" key={item.key}>
+     <p>
+         <input type="text" id={item.key} value={item.text} onChange={(e)=>{
+             props.setUpdate(e.target.value,item.key)}}/>
+        <span>
+       
+        <FontAwesomeIcon className="faicons" onClick={() => {
+            props.deleteItem(item.key)
+        }} icon="trash" />
+        </span>
+     </p>
+     
+    </div>})
+    return <div>
+        <FlipMove duration={300} easing="ease-in-out">
+        {lists}
+        </FlipMove>
+    
+    </div>;
+  }
+
+  export default Lists;
